20608d8e3193a18124dc27fa80b46cf56c49f213
SunKang <coolshark248@gmail.com>
2024-03-24 16:23:40 -0600

    FirstAdd

---
+++ QRcodePractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRcodePractice
+//
+//  Created by 강성찬 on 2024-03-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRcodePractice/QRcodePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRcodePracticeApp.swift
+//  QRcodePractice
+//
+//  Created by 강성찬 on 2024-03-24.
+//
+
+import SwiftUI
+
+@main
+struct QRcodePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
